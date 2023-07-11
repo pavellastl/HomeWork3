d306fbda834a725bb70f539ea45a5c689b1794f3
Pavel <pavellast@mail.ru>
2023-07-11 18:06:51 +0300

    done

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.WriteLine("напишите число N");
+int N = int.Parse(Console.ReadLine()!);
+for(int i = 1; i <= N; i++)
+{
+    Console.WriteLine($"{i} в кубе = {Math.Pow(i,3)}");
+}
+
+    
